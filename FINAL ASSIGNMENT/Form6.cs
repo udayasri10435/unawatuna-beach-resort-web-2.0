b3a18e0b360a7c5507447ee604ad67da4e30df21
udayasri10435 <udayasrimadhushan@gmail.com>
2025-10-05 22:45:45 +0530

    Initial commit

---
+++ FINAL ASSIGNMENT/Form6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FINAL_ASSIGNMENT
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Form4 newform = new Form4();
+            newform.Show();
+            this.Hide();
+
+        }
+    }
+}
